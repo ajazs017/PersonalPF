53348555e8aa6a6033d91c12ff1193118b5ed1e1
Mohammad Ajaz Shaikh <105642900+ajazs017@users.noreply.github.com>
2022-12-10 12:16:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const form = document.querySelector('.form');
+const submitted = document.querySelector('.validate');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -23,3 +25,4 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
